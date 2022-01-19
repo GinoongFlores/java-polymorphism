a35c0209a0fe3f2e609fe2716cbd92973b348346
ginoongflores <flores.135577@cdo.sti.edu.ph>
2022-01-19 21:20:10 +0800

    added the part 2 of the activity

--- src/nine_handson_activity/RunQuad.java
+++ src/nine_handson_activity/RunQuad.java
@@ -16,31 +16,68 @@ class Square extends Rectangle {
     }
 }
 
-class Rectangle extends Quadrilateral {
+class Rectangle extends Parallelogram {
     void showDescription() {
         System.out.println("- has 4 right angles");
         super.showDescription();
     }
 }
 
+class Parallelogram extends Quadrilateral {
+    void showDescription() {
+        System.out.println("- has 2 pairs of parallel sides");
+        super.showDescription();
+    }
+}
 
+class Rhombus extends Parallelogram {
+    void showDescription() {
+        System.out.println("- has 4 congruent sides");
+        super.showDescription();
+    }
+}
+
+class Trapezoid extends Quadrilateral {
+    void showDescription() {
+        System.out.println("- has 1 pair of parallel sides");
+        super.showDescription();
+    }
+}
 
 public class RunQuad {
     public static void main(String[] args) {
         Quadrilateral re = new Rectangle();
         Quadrilateral sq = new Square();
+        Quadrilateral pa = new Parallelogram();
+        Quadrilateral rh = new Rhombus();
+        Quadrilateral tr = new Trapezoid();
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Press R for Rectangle or S for Square. ");
+        System.out.print("""
+                Select from the following:\s
+                R - Rectangle\s
+                S - Square\s
+                P - Parallelogram\s
+                H - Rhombus\s
+                T - Trapezoid\s
+                """);
         String choice = sc.next();
 
         if(choice.equalsIgnoreCase("S")) {
             System.out.println("A square: ");
             sq.showDescription();
-
         } else if (choice.equalsIgnoreCase("R")) {
             System.out.println("A rectangle: ");
             re.showDescription();
+        } else if (choice.equalsIgnoreCase("P")) {
+            System.out.println("A parallelogram: ");
+            pa.showDescription();
+        } else if (choice.equalsIgnoreCase("H")) {
+            System.out.println("A rhombus: ");
+            rh.showDescription();
+        } else if (choice.equalsIgnoreCase("T")) {
+            System.out.println("A trapezoid: ");
+            tr.showDescription();
         }
 
     }
